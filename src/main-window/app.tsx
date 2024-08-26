97292545d9d3cc59f78eb9fbefb64e6383a918be
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-08-26 22:15:05 +0200

    development quality of life

--- src/main-window/app.tsx
+++ src/main-window/app.tsx
@@ -86,7 +86,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/" element={<LandingPage />} /> */}
         <Route
           path="/file"
           element={<FileSelectionPage selectedFile={file} setFile={setFile} />}
@@ -129,7 +129,11 @@ export default function App() {
           path="/clustering"
           element={<ProgressPage startTime={startTime} />}
         />
-        <Route path="/results" element={<ResultsPage />} />
+        <Route
+          // path="/results"
+          path="/"
+          element={<ResultsPage />}
+        />
       </Routes>
     </Router>
   );
