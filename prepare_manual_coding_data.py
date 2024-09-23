f8e3493ad60b7ea8e338d0b7c6128ce7ea59bc49
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2024-09-23 22:15:35 +0200

    update prep example data script

--- prepare_manual_coding_data.py
+++ prepare_manual_coding_data.py
@@ -1,9 +1,11 @@
-import random
 import csv
+import random
 
 INPUT_PATH = "./example_data/example.csv"
-OUTPUT_PATH = "./example_data/example_prepared.csv"
+OUTPUT_PATH = "./example_data/prep_example_automatic.csv"
+MANUAL_PATH = "./example_data/prep_example_manual.csv"
 DELIMITER = ";"
+RESPONSE_COUNT = 100
 
 with open(INPUT_PATH, "r") as f:
     reader = csv.reader(f, delimiter=DELIMITER)
@@ -11,17 +13,28 @@ with open(INPUT_PATH, "r") as f:
     lines = list(reader)
 
 
-# Ignore column 0
-lines = [line[1:] for line in lines]
-
-# Flatten list
-entries = [item for sublist in lines for item in sublist if item]
+# SCG Play Reasons
+lines = [line[7:] for line in lines]
 
 # Shuffle list
-random.shuffle(entries)
+random.shuffle(lines)
+for line in lines:
+    random.shuffle(line)
+
+# Remove empty entries
+lines = [line for line in lines if any(line)]
 
 # Write to file
 with open(OUTPUT_PATH, "w") as f:
+    writer = csv.writer(f, delimiter=",", lineterminator="\n")
+    writer.writerow(headers[7:])
+    writer.writerows(lines[: int(RESPONSE_COUNT / 3)])
+
+# Flatten list
+entries = [item for sublist in lines for item in sublist if item][:RESPONSE_COUNT]
+
+# Write to file
+with open(MANUAL_PATH, "w") as f:
     writer = csv.writer(f, delimiter=",", lineterminator="\n")
     writer.writerow(["entry", "cluster"])
     writer.writerows([[entry, ""] for entry in entries])
